130f00619ddf4dcf34249a423871c7e23f8ce540
Homelicat <homelicat@proton.me>
2024-03-10 21:42:55 +0100

    My header. Test i2c.

--- main.c
+++ main.c
@@ -1,71 +1,77 @@
-#include "stm8.h"
 #include <string.h>
 #include <stdint.h>
-#define UART_SEND(data) while(!((UART1_SR)&(1<<7))); UART1_DR = data
+#include "stm8.h"
+
+#define F_MASTER 16000000UL
+#define UART_BAUD(baud) uart->brr2 = (((F_MASTER/baud) & 0xF000)>>8)|((F_MASTER/baud) & 0x000F); uart->brr1 = ((F_MASTER/baud) >> 4) & 0x00FF
+//#define UART_CHECK() (UART->SR&(1<<5))>0
+#define UART_RECV() uart->dr
+#define UART_SEND(data) while(!((uart->sr)&(1<<7))); uart->dr = data
+#define GPIO_OUT(port,pin) port->ddr|=1<<pin
+#define GPIO_IN(port,pin) port->ddr&=~(1<<pin)
+#define GPIO_PULLUP(port,pin) port->cr1|=1<<pin
+#define GPIO_FLOAT(port,pin) port->cr1&=~(1<<pin)
+#define GPIO_FAST(port,pin) port->cr2|=1<<pin
+#define GPIO_SLOW(port,pin) port->cr2&=~(1<<pin)
+#define GPIO_INT(port,pin) port->cr2|=1<<pin
+#define GPIO_NOINT(port,pin) port->cr2&=~(1<<pin)
+#define GPIO_SET(port,pin,state) port->odr ^=((state>0)<<pin)
+#define GPIO_GET(port,pin) ((port->idr&(1<<pin))>0)
 
 unsigned int millis = 0;
 
 void tim2_update(void) __interrupt(13)
 {
 	millis+=1;
-	TIM2_SR1 &= ~1;
-}
-
-int uart_write(const char *str) {
-    char i;
-    for(i = 0; i < strlen(str); i++) {
-        while(!(UART1_SR & UART_SR_TXE)); // !Transmit data register empty
-        UART1_DR = str[i];
-    }
-    return(i); // Bytes sent
+	tim2->sr1 &= ~1;
 }
 
 void init_uart()
 {
-	UART1_CR2 |= UART_CR2_TEN;
-  UART1_BRR2 = 0x03; UART1_BRR1 = 0x68;
+	uart->cr2 |= (1<<3);
+	UART_BAUD(9600);
 }
 
 void init_tim2()
 {
-	TIM2_ARRH=0x3e;
-	TIM2_ARRL=0x80;
-	TIM2_IER|=1;
-	TIM2_CR1|=1;
+	tim2->arrh=0x3e;
+	tim2->arrl=0x80;
+	tim2->ier|=1;
+	tim2->cr1|=1;
 }
 
 void init_i2c()
 {
-	I2C_FREQR=16;
-	I2C_CCRL=0x50;
-	I2C_TRISER=17;
-	I2C_CR1=1;
+	i2c->freqr=16;
+	i2c->ccrl=0x50;
+	i2c->triser=17;
+	i2c->cr1=1;
 }
 
 void test_i2c()
 {
-	I2C_CR2|=1;
-  while (!(I2C_SR1&1));
-	I2C_DR=0x3C<<1;
-	while (!(I2C_SR1&2));
-	I2C_CR2=2;
-  while (I2C_SR3&1);
-	uart_write("hel\n\r");
+	i2c->cr2|=1;
+  while (!(i2c->sr1&1));
+	i2c->dr=0x3C<<1;
+	while (!(i2c->sr1&2));
+	i2c->cr2=2;
+  while (i2c->sr3&1);
+	//UART_SEND('b');
 }
 
 
 void main()
 {
-	CLK_CKDIVR = 0;
+	clk->ckdivr = 0;
 	init_tim2();
 	init_uart();
 	init_i2c();
-	enableInterrupts();
+	//int_all();
 	int d;
 	while(1)
 	{
 		d = millis;
 		while(millis-d<2000);
-		//test_i2c();
+		test_i2c();
 	}
 }
